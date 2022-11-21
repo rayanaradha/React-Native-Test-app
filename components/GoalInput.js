3d7fbe9b29fc0f0bba5f6aa2101ee8eaaf44c2dc
rayanaradha <rayanaradha1993@gmail.com>
2022-11-21 14:48:08 +0530

    adding a image

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
-import { StyleSheet, View , TextInput, Button, Modal} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from 'react-native';
 
 export default function GoalInput(props) {
   const [goal, setGoal] = useState('');
@@ -20,49 +27,54 @@ export default function GoalInput(props) {
   }
 
   return (
-    <Modal visible={props.showModel} animationType='slide'>
+    <Modal visible={props.showModel} animationType="slide">
       <View style={styles.TextInputcontainer}>
-          <TextInput
-            style={styles.TextInput}
-            value = {goal}
-            onChangeText={goalInputHnadler}
-            placeholder="Your course goal!"
-          />
-           <View style={styles.ButtonContainer}>
-              <View style={styles.Button}>
-                <Button title="Add Goal" onPress={onAddGoalHandler} />
-              </View>
-              <View style={styles.Button}>
-                <Button title="Cancel" onPress={onCloseGoalHandler} />
-             </View>
+        <Image source={require('../assets/images/goal.png')} style={styles.image} />
+        <TextInput
+          style={styles.TextInput}
+          value={goal}
+          onChangeText={goalInputHnadler}
+          placeholder="Your course goal!"
+        />
+        <View style={styles.ButtonContainer}>
+          <View style={styles.Button}>
+            <Button title="Add Goal" onPress={onAddGoalHandler} />
           </View>
+          <View style={styles.Button}>
+            <Button title="Cancel" onPress={onCloseGoalHandler} />
+          </View>
+        </View>
       </View>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
- TextInputcontainer: {
+  TextInputcontainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    margingBottom: 20,
-    borderBottomWidth: 1,
-    padding: 16, 
-    borderColor: '#cccccc',
+    padding: 16,
+    backgroundColor: '#311b6b',    
     flex: 1,
   },
   TextInput: {
-      borderWidth: 1,
-      width: '100%',
-      borderColor: '#cccccc',
-      padding: 8,
-    },
+    borderWidth: 1,
+    width: '100%',
+    borderColor: '#cccccc',
+    padding: 8,
+    color: 'white'
+  },
   ButtonContainer: {
     flexDirection: 'row',
     marginTop: 10,
   },
-  Button:{
+  Button: {
     width: 100,
     marginHorizontal: 8,
-  }  
+  },
+  image:{
+    width:100,
+    height:100,
+    margin:20,
+  }
 });
